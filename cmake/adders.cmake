73963c2ba440ce0c77db0449253d420853119402
maciejjablonsky <maciekjablonsky@gmail.com>
2021-07-17 21:23:29 +0200

    Added executable creator helper

--- cmake/adders.cmake
+++ cmake/adders.cmake
@@ -1,4 +1,4 @@
-function(add_static_library LIBRARY_NAME)
+function(utils_add_static_library LIBRARY_NAME)
 	set(SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src)
     if (NOT EXISTS ${SRC_DIR})
         message(FATAL_ERROR "Source directory for '${LIBRARY_NAME}' static library doesn't exist. Should be: ${SRC_DIR}")
@@ -26,13 +26,37 @@ function(add_static_library LIBRARY_NAME)
 
     add_library(${LIBRARY_NAME} STATIC ${PARSED_ARGUMENTS_SOURCES} ${PARSED_ARGUMENTS_PUBLIC_HEADERS} ${PARSED_ARGUMENTS_PRIVATE_HEADERS})
 
-    target_include_directories(${LIBRARY_NAME} PUBLIC ${PARSED_ARGUMENTS_PUBLIC_HEADERS})
-    target_include_directories(${LIBRARY_NAME} PRIVATE ${PARSED_ARGUMENTS_PRIVATE_HEADERS})
+    target_include_directories(${LIBRARY_NAME} PUBLIC ${PUBLIC_HEADERS_DIR})
 
     if (PARSED_ARGUMENTS_PUBLIC_LIBS)
         target_link_libraries(${LIBRARY_NAME} PUBLIC ${PARSED_ARGUMENTS_PUBLIC_LIBS})
     endif()
     if (PARSED_ARGUMENTS_PRIVATE_LIBS)
         target_link_libraries(${LIBRARY_NAME} PRIVATE ${PARSED_ARGUMENTS_PRIVATE_LIBS})
+    endif()
+endfunction()
+
+function(utils_add_executable EXECUTABLE_NAME)
+	set(SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src)
+    if (NOT EXISTS ${SRC_DIR})
+        message(FATAL_ERROR "Source directory for '${EXECUTABLE_NAME}' executable doesn't exist. Should be: ${SRC_DIR}")
+    endif()
+
+    cmake_parse_arguments(
+        PARSED_ARGUMENTS # prefix for inserted variables
+        "" # list of names of the boolean arguments (defined are set to true)
+        "" # mono valued arguments
+        "SOURCES;LIBS" # names of list-valued arguments
+        ${ARGN}
+    )
+
+    if (NOT PARSED_ARGUMENTS_SOURCES)
+        message(FATAL_ERROR "No source files for executable '${EXECUTABLE_NAME}' added.")
+    endif()
+
+    add_executable(${EXECUTABLE_NAME} ${PARSED_ARGUMENTS_SOURCES})
+
+    if (PARSED_ARGUMENTS_LIBS)
+        target_link_libraries(${EXECUTABLE_NAME} PUBLIC ${PARSED_ARGUMENTS_LIBS})
     endif()
 endfunction()
