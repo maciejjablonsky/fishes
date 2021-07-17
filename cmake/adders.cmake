ed5b0c92a1cd224729ffe64b39e43a9f2a85774a
maciejjablonsky <maciekjablonsky@gmail.com>
2021-07-17 21:01:56 +0200

    Fixed adder function

--- cmake/adders.cmake
+++ cmake/adders.cmake
@@ -1,22 +1,26 @@
 function(add_static_library LIBRARY_NAME)
 	set(SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src)
-    if (not exists ${SRC_DIR})
+    if (NOT EXISTS ${SRC_DIR})
         message(FATAL_ERROR "Source directory for '${LIBRARY_NAME}' static library doesn't exist. Should be: ${SRC_DIR}")
     endif()
 
     set(PUBLIC_HEADERS_DIR ${CMAKE_CURRENT_SOURCE_DIR}/include)
-    if (not exists ${PUBLIC_HEADERS_DIR})
+    if (NOT EXISTS ${PUBLIC_HEADERS_DIR})
         message(FATAL_ERROR "Include directory for '${LIBRARY_NAME}' static library doesn't exist. Should be: ${PUBLIC_HEADERS_DIR}")
+    endif()
+    if (NOT EXISTS ${PUBLIC_HEADERS_DIR}/${LIBRARY_NAME})
+        message(FATAL_ERROR "Library '${LIBRARY_NAME}' doesn't export public headers under include/lib_name path. Should be '${PUBLIC_HEADERS_DIR}/${LIBRARY_NAME}'")
     endif()
 
     cmake_parse_arguments(
         PARSED_ARGUMENTS # prefix for inserted variables
         "" # list of names of the boolean arguments (defined are set to true)
+        "" # mono valued arguments
         "PUBLIC_HEADERS;PRIVATE_HEADERS;SOURCES;PUBLIC_LIBS;PRIVATE_LIBS" # names of list-valued arguments
         ${ARGN}
     )
 
-    if (not PARSED_ARGUMENTS_SOURCES)
+    if (NOT PARSED_ARGUMENTS_SOURCES)
         message(FATAL_ERROR "No source files for static library '${LIBRARY_NAME}' added.")
     endif()
 
