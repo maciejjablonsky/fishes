1cf5f2261c466b01d360d5fcb494efb8e9f8de01
maciejjablonsky <maciekjablonsky@gmail.com>
2021-08-03 21:36:35 +0200

    Renaming to lake

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class LakeConan(ConanFile):
     requires = 'fmt/8.0.1', 'entt/3.8.0', 'glfw/3.3.4', 'glm/0.9.9.8', 'boost/1.76.0', 'glad/0.1.34'
 
     def source(self):
-        self.run('git clone https://github.com/maciejjablonsky/fishes.git')
+        self.run('git clone https://github.com/maciejjablonsky/lake.git')
 
     def build(self):
         cmake = CMake(self)
