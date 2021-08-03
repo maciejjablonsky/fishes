e4b45d794b8c6f7d2addc5b1c6370ebcc0debd99
maciejjablonsky <maciekjablonsky@gmail.com>
2021-08-03 23:53:23 +0200

    Added more settings to cmake-format

--- .cmake-format.py
+++ .cmake-format.py
@@ -1,4 +1,9 @@
 with section("format"):
-    line_width=80
-    tab_size=4
+    line_width = 80
+    tab_size = 4
     dangle_parens = False
+    enable_sort = True
+    command_case = 'canonical'
+    keyword_case = 'upper'
+    max_subgroups_hwrap = 6
+    max_pargs_hwrap = 2
